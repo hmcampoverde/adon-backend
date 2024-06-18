c46628dc6585fe6f6fb0ae0972ecc15700afa331
Henry Manuel Campoverde Hidalgo <hmcampoverde@gmail.com>
2024-06-18 11:43:46 -0500

    Agregar metodos constructores

--- src/main/java/org/hmcampoverde/dto/EmployeeRegisterDto.java
+++ src/main/java/org/hmcampoverde/dto/EmployeeRegisterDto.java
@@ -2,11 +2,15 @@ package org.hmcampoverde.dto;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class EmployeeRegisterDto {
 
     private Long id;
